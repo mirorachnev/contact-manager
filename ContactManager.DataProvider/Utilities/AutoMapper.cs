fa982f8f4aa9a0da58380592354ceec425267b39
Miroslav Rachnev <mrachnev@yahoo.com>
2022-04-12 14:08:45 +0300

    Add automapper

---
+++ ContactManager.DataProvider/Utilities/AutoMapper.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using ContactManager.DataProvider.DbData;
+using ContactManager.MessageBus.Messages.DataTypes;
+
+namespace ContactManager.DataProvider.Utilities
+{
+    /// <summary>
+    /// Auto mapper
+    /// </summary>
+    internal static class AutoMapper
+    {
+        private static IMapper _autoMapper;
+
+        /// <summary>
+        /// Static constructor that creates mapper instance internally.
+        /// </summary>
+        static AutoMapper()
+        {
+            _autoMapper = CreateMapper();
+        }
+
+        /// <summary>
+        /// Gets mapper insatnce for type mapping.
+        /// </summary>
+        public static IMapper GetMapper()
+        {
+            return _autoMapper;
+        }
+
+        /// <summary>
+        /// Creates mapper for type mapping.
+        /// </summary>
+        private static IMapper CreateMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AllowNullCollections = true;
+                cfg.AllowNullDestinationValues = true;
+
+                cfg.CreateMap<Contact, ContactData>().ReverseMap();
+            });
+            
+            _autoMapper = config.CreateMapper();
+            return _autoMapper;
+        }
+    } 
+}
