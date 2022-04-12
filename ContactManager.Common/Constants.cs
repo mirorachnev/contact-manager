34fb6870a1140ef220b0e641b6c1225ee8effcc0
Miroslav Rachnev <mrachnev@yahoo.com>
2022-04-12 12:08:09 +0300

    Add shared data project

---
+++ ContactManager.Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace ContactManager.Common
+{
+    /// <summary>
+    /// Shared constants
+    /// </summary>
+    public class Constants
+    {
+        // Message bus connection string
+        public const string RabbitMqConnectionString = "amqp://guest:guest@localhost:5672";
+    }
+}
