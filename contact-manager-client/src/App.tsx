487abb1f096966b216731d2c8e985c8918699e15
Miroslav Rachnev <mrachnev@yahoo.com>
2022-04-14 12:23:24 +0300

    Add routing and components

--- contact-manager-client/src/App.tsx
+++ contact-manager-client/src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Home } from './components/Home';
+import { Contacts } from './components/Contacts';
+import { Navigation } from './components/Navigation';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <h3 className="m-3 d-flex justify-content-center">
+          React JS Tutorial
+        </h3>
+
+        <Navigation />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contacts' element={<Contacts />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ contact-manager-client/src/components/Contacts.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Contacts extends Component {
+
+  render() {
+    return (
+      <div className="mt-5 d-flex justify-content-left">
+        This is contacts page.
+      </div>
+    )
+  }
+}
