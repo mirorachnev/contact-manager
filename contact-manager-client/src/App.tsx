90d0f81b8e9e84f345b7871aef484a27adc6afcb
Miroslav Rachnev <mrachnev@yahoo.com>
2022-04-15 14:00:41 +0300

    Remove routing

--- contact-manager-client/src/App.tsx
+++ contact-manager-client/src/App.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
 import './App.css';
 
-import { Home } from './components/Home';
 import { ContactList } from './components/ContactList';
-import { Navigation } from './components/Navigation';
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="container">
-        <h3 className="m-3 d-flex justify-content-center">
-          React JS Tutorial
-        </h3>
+    <div className="container">
+      <h3 className="m-3 d-flex justify-content-center">
+        Contact List
+      </h3>
 
-        <Navigation />
+      <br />
 
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/contacts' element={<ContactList />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+      <ContactList />
+    </div>
   );
 }
 
